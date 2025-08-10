7166573af52fa873963d4df2b3ec6fff64087e61
ElHadji11 <babacar.thior19@gmail.com>
2025-08-10 15:09:14 +0000

    posts and routes first fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,48 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import { ENV } from './config/env.js';
+import cors from 'cors';
+import { clerkMiddleware } from "@clerk/express";
+import { arcjetMiddleware } from "./middlewares/arcjet.middleware.js";
+
+
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+
+// import commentRoutes from "./routes/comment.route.js";
+// import notificationRoutes from "./routes/notification.route.js";
+
 
 const app = express();
 
-app.listen(3000, () => (
-    console.log('Server is running on port 3000')
-))
+
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
+// app.use(arcjetMiddleware);
+
+app.get("/", (req, res) => res.send('Hello World!'));
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/notifications", notificationRoutes);
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        // listen for local development
+        if (ENV.NODE_ENV !== "production") {
+            app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+        }
+    } catch (error) {
+        console.error("Failed to start server:", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
+
+export default app;
